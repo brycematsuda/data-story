940709d34e963e7fa05e2adf02755d7e21402aeb
Bryce Matsuda <iexist4jcore@gmail.com>
2015-06-28 21:11:03 -0700

    Add AngularJS support, update server jsons.

--- js/data-story.js
+++ js/data-story.js
@@ -1,37 +1,15 @@
-var list = [];
+angular.module('data-story', []).controller('ServerController', function($scope) {
 
-var server = "grazed";
+  // Current list of GMS servers
+  $scope.servers = ["Scania", "Windia", "Bera", "KHROA", "MYBCKN", "GRAZED"];
 
-$.getJSON("json/" + server + ".json", function(data) {
-  console.log(data);
-  _.each(data[0].fm_items, function(item, key){
-    var x = _.findWhere(list, {"name" : item["O"]});
-    if (x){
-      x["amount"] += parseInt(item["a"]);
-    }
-    else {
-      if (item["Q"] === "Equip") list.push({"name" : item["O"], "amount" : parseInt(item["a"])});
-    }
-  });
+  // Index of server selected
+  $scope.selectedIdx = 0;
 
-  list = _.sortBy(list, 'amount').reverse();
-});
+  // Current chart being displayed
+  $scope.chart = new google.visualization.BarChart(document.getElementById('bar_chart'));
 
- google.load('visualization', '1', {packages: ['corechart', 'bar']});
- google.setOnLoadCallback(drawAxisTickColors);
-
- function drawAxisTickColors() {
-
-  var graphData = [];
-  graphData.push(['Item', 'Amount']);
-
-  for (var i = 0; i < 11; i++){
-    graphData.push([list[i]["name"], list[i]["amount"]]);
-  }
-
-
-  var data = google.visualization.arrayToDataTable(graphData);
-
+  // Chart options
   var options = {
     title: 'Top 10 Items For Sale In Free Market',
     width: 1200,
@@ -41,8 +19,7 @@ $.getJSON("json/" + server + ".json", function(data) {
       minValue: 0,
       textStyle : {
         fontSize : 14
-      },
-      ticks: [10, 20, 30, 40, 50]
+      }
     },
     vAxis: { 
       gridlines: { count: 5 },
@@ -52,7 +29,52 @@ $.getJSON("json/" + server + ".json", function(data) {
     },
     bar: {groupWidth: "90%"}
   };
-  var chart = new google.visualization.BarChart(document.getElementById('bar_chart'));
-  chart.draw(data, options);
-}
 
+  // "Cached" views to be reused later (instead of parsing the json every time)
+  var views = {};
+
+  // Called when a user clicks on a server nav pill
+  $scope.serverClicked = function ($index){
+    $scope.selectedIdx = $index;
+    var server = $scope.servers[$index].toLowerCase();
+
+    // If the chart for the server doesn't already exist,
+    // parse the json and make a new one.
+    if (views[server] === undefined){
+      var list = $scope.get(server);
+      var graphData = [];
+      graphData.push(['Item', 'Amount']);
+
+      for (var i = 0; i < 11; i++){
+        graphData.push([list[i]["name"], list[i]["amount"]]);
+      }
+
+      // Otherwise, load what we already have
+      views[server] = google.visualization.arrayToDataTable(graphData);
+    }
+
+    // Draw the graph
+    $scope.chart.draw(views[server], options);
+
+  };
+
+  // Grabs the JSON and returns a list based on defined params
+  // (for now, it just returns the amount of each equip items being sold from highest to lowest)
+  $scope.get = function(server){
+    $.getJSON("json/" + server + ".json", function(data) {
+      _.each(data[0].fm_items, function(item, key){
+        var i = _.findWhere(list, {"name" : item["O"]});
+        if (i){
+          i["amount"] += parseInt(item["a"]);
+        }
+        else {
+          if (item["Q"] === "Equip") list.push({"name" : item["O"], "amount" : parseInt(item["a"])});
+        }
+      });
+
+      list = _.sortBy(list, 'amount').reverse();
+    });
+
+    return list;
+  }
+});
---
+++ js/init.js
@@ -0,0 +1,55 @@
+// Inital graph that loads on first load.
+
+var list = [];
+var server = "scania";
+
+$.getJSON("json/" + server + ".json", function(data) {
+  _.each(data[0].fm_items, function(item, key){
+    var x = _.findWhere(list, {"name" : item["O"]});
+    if (x){
+      x["amount"] += parseInt(item["a"]);
+    }
+    else {
+      if (item["Q"] === "Equip") list.push({"name" : item["O"], "amount" : parseInt(item["a"])});
+    }
+  });
+
+  list = _.sortBy(list, 'amount').reverse();
+});
+
+ google.load('visualization', '1', {packages: ['corechart', 'bar']});
+ google.setOnLoadCallback(drawAxisTickColors);
+
+ function drawAxisTickColors() {
+
+  var graphData = [];
+  graphData.push(['Item', 'Amount']);
+
+  for (var i = 0; i < 11; i++){
+    graphData.push([list[i]["name"], list[i]["amount"]]);
+  }
+
+  var data = google.visualization.arrayToDataTable(graphData);
+
+  var options = {
+    title: 'Top 10 Items For Sale In Free Market',
+    width: 1200,
+    height: 600,
+    hAxis: {
+      title: 'Amount',
+      minValue: 0,
+      textStyle : {
+        fontSize : 14
+      }
+    },
+    vAxis: { 
+      gridlines: { count: 5 },
+      textStyle : {
+        fontSize : 14
+      }
+    },
+    bar: {groupWidth: "90%"}
+  };
+  var chart = new google.visualization.BarChart(document.getElementById('bar_chart'));
+  chart.draw(data, options);
+}
